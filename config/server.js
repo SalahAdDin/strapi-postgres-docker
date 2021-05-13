fd8881d02c5ad419e36fdedb7a2d6da86b8f3a92
Luis Alaguna <alagunasalahaddin@live.com>
2021-05-13 14:29:25 +0300

    :tada: initial setup.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '65c8acf27601c21b11e23f445729edb1'),
+    },
+  },
+});
